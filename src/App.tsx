b0f08bd9a642e12e6525abc19f412f146a487c80
sarthakD09 <sarthakdatir22@gmail.com>
2025-06-22 14:55:48 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="lead-gen-form-drab.vercel.app">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
